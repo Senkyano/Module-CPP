563558238de65157e9917aeb4541a22abf2c1154
Senkyano <rithyyano.hoy@gmail.com>
2024-05-12 12:36:54 +0200

    first ex00

---
+++ CPP00-04/CPP00/Ex00/srcs/megaphone.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/12 11:43:05 by rihoy             #+#    #+#             */
+/*   Updated: 2024/05/12 12:36:30 by rihoy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	while (++i < argc)
+	{
+		j = -1;
+		while (argv[i][++j])
+			std::cout << (char)toupper(argv[i][j]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
