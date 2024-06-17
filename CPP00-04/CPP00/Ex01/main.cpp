628a1c60eb7106155c67367be802d82b00524157
Rithy-Yano Hoy <rithyyano.hoy@gmail.com>
2024-06-17 12:54:59 +0200

    corriger CPP00

--- CPP00-04/CPP00/Ex01/main.cpp
+++ CPP00-04/CPP00/Ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 12:39:39 by rihoy             #+#    #+#             */
-/*   Updated: 2024/05/28 11:42:05 by rihoy            ###   ########.fr       */
+/*   Updated: 2024/06/17 12:53:58 by rihoy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,16 @@ int	main(int argc, char **argv)
 	{
 		print_menu();
 		while (1)
-		
 		{
 			std::cout << PUR << "	╚> " << RST;
 			std::getline(std::cin, input);
-			if (input.empty())
+			if (input.empty() && !std::cin.eof())
 			{
 				std::cout << RED << "	   Please enter a command" << RST << std::endl;
-				
+			}
+			else if (std::cin.eof() || std::cin.fail())
+			{
+				return (0);
 			}
 			if (strcmp(input.c_str(), "EXIT") == 0)
 				return (0);
@@ -53,7 +55,9 @@ int	main(int argc, char **argv)
 				break ;
 			}
 			else
+			{
 				std::cout << RED << "	   Invalid command" << RST << std::endl;
+			}
 		}
 	}
 	return (0);
