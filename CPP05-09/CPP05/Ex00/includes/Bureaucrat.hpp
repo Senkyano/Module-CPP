8074ad50ad8835d565042b9704cc75af0a6a4020
Senkyano <rithyyano.hoy@gmail.com>
2024-08-18 23:11:02 +0200

    bureaucrate

--- CPP05-09/CPP05/Ex00/includes/Bureaucrat.hpp
+++ CPP05-09/CPP05/Ex00/includes/Bureaucrat.hpp
@@ -6,13 +6,31 @@
 /*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 13:17:39 by rihoy             #+#    #+#             */
-/*   Updated: 2024/08/18 13:18:10 by rihoy            ###   ########.fr       */
+/*   Updated: 2024/08/18 19:24:01 by rihoy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
 
+#include <iostream>
+#include "main.hpp"
 
+class	Bureaucrat
+{
+	private :
+		std::string const	name;
+		int				grade;
+
+	public :
+		Bureaucrat(std::string const name, int grade);
+		~Bureaucrat();
+
+		std::string const	getName() const;
+		int					getGrade() const;
+		void				setGrade(int grade);
+		void				incrementGrade();
+		void				decrementGrade();
+};
 
 #endif
