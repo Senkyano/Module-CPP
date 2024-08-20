afdab8ebda33a00caaa28c741c636bb6e8b3fe7f
Senkyano <rithyyano.hoy@gmail.com>
2024-08-20 13:43:09 +0200

    upd CPP05

---
+++ CPP05-09/CPP05/Ex02/srcs/PresidentialPardonForm.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.cpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/20 13:35:45 by rihoy             #+#    #+#             */
+/*   Updated: 2024/08/20 13:41:50 by rihoy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PresidentialPardonForm.hpp"
+
+PresidentialPardonForm::PresidentialPardonForm(std::string const target) : AForm("PresidentialPardonForm", 25, 5), target(target)
+{
+}
+
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+}
+
+void	PresidentialPardonForm::execute(Bureaucrat const &executor) const
+{
+	try
+	{
+		if (executor.getGrade() > this->getGradeExec())
+			throw AForm::GradeTooLowException();
+		else if (executor.getGrade() < 1)
+			throw AForm::GradeTooHighException();
+		
+	}
+	catch (std::exception &e)
+	{
+		
+	}
+}
