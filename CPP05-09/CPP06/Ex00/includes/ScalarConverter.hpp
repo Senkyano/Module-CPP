f1b91da9c7cdb671b0b6d71849bf88ea58e42b5f
Senkyano <rithyyano.hoy@gmail.com>
2024-08-20 22:33:44 +0200

    add CPP06

---
+++ CPP05-09/CPP06/Ex00/includes/ScalarConverter.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/20 22:29:28 by rihoy             #+#    #+#             */
+/*   Updated: 2024/08/20 22:33:05 by rihoy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+# include <iostream>
+
+class ScalarConverter
+{
+	private:
+	public:
+};
+
+#endif
