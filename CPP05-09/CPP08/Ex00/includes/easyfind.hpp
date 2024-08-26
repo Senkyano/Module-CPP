a59951fdc851d3a2681746923a34a6369e798c82
Senkyano <rithyyano.hoy@gmail.com>
2024-08-26 14:55:16 +0200

    CPP08

---
+++ CPP05-09/CPP08/Ex00/includes/easyfind.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/24 23:25:05 by rihoy             #+#    #+#             */
+/*   Updated: 2024/08/24 23:27:49 by rihoy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+
+template <typename T>
+int	easyfind(T &container, int n)
+{
+	
+}
+
+#endif
