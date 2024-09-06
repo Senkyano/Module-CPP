704308ef5898281504dd345e1ac2c438d8ee9f5b
Senkyano <rithyyano.hoy@gmail.com>
2024-09-06 21:07:28 +0200

    CPP08 ex02

---
+++ CPP05-09/CPP08/Ex02/includes/MutantStack.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 21:52:59 by rihoy             #+#    #+#             */
+/*   Updated: 2024/09/05 22:02:02 by rihoy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include "main.hpp"
+# include <stack>
+
+template<typename T>
+class	MutantStack : public std::stack<T>
+{
+	public :
+		//constructeur
+			
+};
+
+#endif
