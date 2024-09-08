ec68421925705d41da21a8b2aa1085068b38c7c6
Senkyano <rithyyano.hoy@gmail.com>
2024-09-08 13:54:24 +0200

    CPP08

--- CPP05-09/CPP08/Ex02/includes/MutantStack.hpp
+++ CPP05-09/CPP08/Ex02/includes/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 21:52:59 by rihoy             #+#    #+#             */
-/*   Updated: 2024/09/05 22:02:02 by rihoy            ###   ########.fr       */
+/*   Updated: 2024/09/07 19:56:39 by rihoy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,16 @@ template<typename T>
 class	MutantStack : public std::stack<T>
 {
 	public :
-		//constructeur
-			
+		// Constructeur
+		MutantStack<T>();
+		MutantStack<T>(const MutantStack<T> &src);
+		
+		// Destructeur
+		~MutantStack<T>();
+
+		// Operator Assign
+		MutantStack<T>&operator=(const MutantStack<T> &src);
+		
 };
 
 #endif
