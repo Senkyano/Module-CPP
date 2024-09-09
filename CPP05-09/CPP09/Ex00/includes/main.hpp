26dba75ea391986b72df58a86326e3bbf9963ad6
Senkyano <rithyyano.hoy@gmail.com>
2024-09-09 22:13:52 +0200

    begin CPP09

---
+++ CPP05-09/CPP09/Ex00/includes/main.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/21 22:16:02 by rihoy             #+#    #+#             */
+/*   Updated: 2024/09/09 20:53:52 by rihoy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_HPP
+# define MAIN_HPP
+
+# include <iostream>
+# include <string>
+# include <cstring>
+# include <cstdlib>
+
+# define BLK "\e[0;90m"
+# define RED "\e[1;31m"
+# define GR "\e[1;92m"
+# define Y "\e[1;93m"
+# define BLU "\e[1;94m"
+# define PUR "\e[1;95m"
+# define CY "\e[1;96m"
+# define WH "\e[0;97m"
+# define RST "\e[0m"
+
+#endif
