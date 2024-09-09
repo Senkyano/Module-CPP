122e47468fb0c1f6620de0bede239eb361565a5d
Rithy-Yano Hoy <rithyyano.hoy@gmail.com>
2024-09-09 15:52:40 +0200

    cpp08/ex02

--- CPP05-09/CPP08/Ex02/includes/MutantStack.hpp
+++ CPP05-09/CPP08/Ex02/includes/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 21:52:59 by rihoy             #+#    #+#             */
-/*   Updated: 2024/09/07 19:56:39 by rihoy            ###   ########.fr       */
+/*   Updated: 2024/09/09 15:35:10 by rihoy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,68 @@ class	MutantStack : public std::stack<T>
 
 		// Operator Assign
 		MutantStack<T>&operator=(const MutantStack<T> &src);
-		
+
+		// Iterateur
+		typedef typename std::stack<T>::container_type::iterator		iterator;
+		typedef typename std::stack<T>::container_type::const_iterator	const_iterator;
+
+		iterator		begin();
+		iterator		end();
+		const_iterator	cbegin();
+		const_iterator	cend();
+		// class parent
+		// using -> permet de reutiliser les methode de la class parent;
+		using	std::stack<T>::push;
+		using	std::stack<T>::pop;
+		using	std::stack<T>::size;
+		using	std::stack<T>::empty;
 };
+
+// Constructeur
+template<typename T>
+MutantStack<T>::MutantStack ()
+{}
+
+template<typename T>
+MutantStack<T>::MutantStack (const MutantStack<T> &src)
+{
+	*this = src;
+}
+
+template<typename T>
+MutantStack<T>::~MutantStack()
+{}
+
+template<typename T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack<T> &src)
+{
+	if (this != &src)
+		this->c = src.c;
+	return (*this);
+}
+
+template<typename T>
+typename std::stack<T>::container_type::iterator	MutantStack<T>::begin()
+{
+	return (this->c.begin());
+}
+
+template<typename T>
+typename std::stack<T>::container_type::iterator	MutantStack<T>::end()
+{
+	return (this->c.end());
+}
+
+template<typename T>
+typename std::stack<T>::container_type::const_iterator	MutantStack<T>::cbegin()
+{
+	return (this->c.cbegin());
+}
+
+template<typename T>
+typename std::stack<T>::container_type::const_iterator	MutantStack<T>::cend()
+{
+	return (this->c.cend());
+}
 
 #endif
--- CPP05-09/CPP08/Ex02/main.cpp
+++ CPP05-09/CPP08/Ex02/main.cpp
@@ -6,11 +6,33 @@
 /*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 21:52:57 by rihoy             #+#    #+#             */
-/*   Updated: 2024/09/07 19:52:38 by rihoy            ###   ########.fr       */
+/*   Updated: 2024/09/09 15:51:34 by rihoy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		main(void)
+#include "MutantStack.hpp"
+
+int main()
 {
-	return (0);
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << "top : " << mstack.top() << std::endl;
+	std::cout << "on retire 1 element de la stack\n";
+	mstack.pop();
+	std::cout << "size stack : " << mstack.size() << std::endl;
+	std::cout << "on rajoute de 0 a 100 element de la stack\n";
+	for (int i = 0; i < 100; ++i)
+		mstack.push(i);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << " ;";
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
 }
